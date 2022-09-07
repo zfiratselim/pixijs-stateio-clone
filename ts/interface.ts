96136cdd012286d4694221dad55ff50ae488a603
Fırat Selim ZİREKOĞLU <zirekoglufs@gmail.com>
2022-09-08 00:25:56 +0300

    added Interface

---
+++ ts/interface.ts
@@ -0,0 +1,14 @@
+import * as PIXI from "pixi.js";
+
+export interface Coords{
+    x:number,
+    y:number
+}
+
+export interface CenterOfState extends PIXI.Sprite{
+    shake:()=>void;
+}
+
+export interface StateInt extends PIXI.Sprite{
+    fire:()=>void;
+}
