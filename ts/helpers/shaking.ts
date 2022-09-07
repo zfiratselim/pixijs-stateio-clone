3bc82839935dc6bc235eb3f716af9eb357dd5d51
Fırat Selim ZİREKOĞLU <zirekoglufs@gmail.com>
2022-09-08 00:26:14 +0300

    added Shaking func

---
+++ ts/helpers/shaking.ts
@@ -0,0 +1,21 @@
+export function shake(center){
+    [
+       { x:1,y:1, rotate:0 },
+       { x:-1,y:-2, rotate:-1 },
+       { x:-3,y:0, rotate:1 },
+       { x:3,y:2, rotate:0 },
+       { x:1,y:-1, rotate:1 },
+       { x:-1,y:2, rotate:-1 },
+       { x:-3,y:1, rotate:0 },
+       { x:3,y:1, rotate:-1 },
+       { x:-1,y:-1, rotate:1 },
+       { x:1,y:2, rotate:0 },
+       { x:1,y:-2, rotate:-1 }
+    ].forEach((e,i)=>{
+        setTimeout(()=>{
+        center.x=e.x;
+        center.y=e.y;
+        center.rotate=e.rotate;
+        },i*100)
+    })
+}
